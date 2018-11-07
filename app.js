552f76ed75acbebf7892c088be0f0efd61b7a433
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-07 15:46:12 +0000

    Inital set-up for app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const { DB_URL } = require('./config');
+const bodyParser = require('body-parser');
+// require in apiRouter here.
+
+mongoose.connect(DB_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`connected to ${DB_URL}`);
+  })
+  .catch(console.log);
+
+app.use(bodyParser.json());
+
+app.use('/api', apiRouter);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+})
+
+module.exports = app;
