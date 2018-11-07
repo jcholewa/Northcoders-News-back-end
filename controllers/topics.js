e2b960df3b63c35d7cae81bec9fdb1fd7609e5b7
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-07 16:20:20 +0000

    Complete getTopics controller function (no tests yet)

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,5 +1,11 @@
-exports.getTopics = () => {
+const { Topic } = require('../models');
 
+exports.getTopics = (req, res, next) => {
+  Topic.find()
+    .then(topics => {
+      res.status(200).send({ topics })
+    })
+    .catch(next)
 }
 
 exports.getArticlesForTopic = () => {
--- routes/topics.js
+++ routes/topics.js
@@ -1,8 +1,12 @@
 const topicsRouter = require('express').Router();
+const { getTopics } = require('../controllers/topics');
+
+topicsRouter.route('/')
+  .get(getTopics)
 
 /*
 endpoints:
-GET /api/topics
+GET /api/topics - done, no tests yets
 GET /api/topics/:topic_slug/articles
 POST /api/topics/:topic_slug/articles
 */
