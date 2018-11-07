18787fa0c46e68cc8160d302555c07e9ce4692de
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-07 16:28:01 +0000

    Complete getOneUser controller function (no tests yet)

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,9 @@
-exports.getOneUser = () => {
+const { User } = require('../models');
 
+exports.getOneUser = (req, res, next) => {
+  const username = req.params.username
+  User.findOne({ username: username })
+    .then(user => {
+      res.status(200).send({ user })
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const usersRouter = require('express').Router();
+const { getOneUser } = require('../controllers/users');
+
+usersRouter.get('/:username', getOneUser)
 
 /*
 endpoint:
-GET /api/users/:username
+GET /api/users/:username - done, no tests yet
 */
 
 module.exports = usersRouter;
