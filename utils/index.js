899ed4e4585c3070ea861102ccb34bbf3325fbb9
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-12-16 19:35:22 +0000

    Remove unneeded console log

--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,23 @@
-const { Comment } = require('../models')
+const { Comment } = require("../models");
 
-const formatTopics = (topicData) => {
+const formatTopics = topicData => {
   return topicData.map(topicDatum => {
-    return { ...topicDatum }
-  })
-}
+    return { ...topicDatum };
+  });
+};
 
-const formatUsers = (userData) => {
+const formatUsers = userData => {
   return userData.map(userDatum => {
-    return { ...userDatum }
-  })
-}
+    return { ...userDatum };
+  });
+};
 
 const createRefObj = (docs, key) => {
   return docs.reduce((refObj, doc, index) => {
-    refObj[doc[key]] = docs[index]._id
+    refObj[doc[key]] = docs[index]._id;
     return refObj;
-  }, {})
-}
+  }, {});
+};
 
 const formatArticles = (articleData, userRefObj) => {
   return articleData.map(articleDatum => {
@@ -26,9 +26,9 @@ const formatArticles = (articleData, userRefObj) => {
       belongs_to: articleDatum.topic,
       created_by: userRefObj[articleDatum.created_by],
       votes: articleDatum.votes || 0
-    }
-  })
-}
+    };
+  });
+};
 
 const formatComments = (commentData, userRefObj, articleRefObj) => {
   return commentData.map(commentDatum => {
@@ -38,43 +38,48 @@ const formatComments = (commentData, userRefObj, articleRefObj) => {
       created_at: commentDatum.created_at,
       created_by: userRefObj[commentDatum.created_by],
       belongs_to: articleRefObj[commentDatum.belongs_to]
-    }
-  })
-}
+    };
+  });
+};
 
 const commentCount = (articleID, article) => {
-  return Comment.count({ belongs_to: articleID })
-    .then(count => {
-      article = article.toJSON();
-      article["comment_count"] = count;
-      return article;
-    })
-}
+  return Comment.count({ belongs_to: articleID }).then(count => {
+    article = article.toJSON();
+    article["comment_count"] = count;
+    return article;
+  });
+};
 
 // creates a reference object for number of mentions (e.g. number of comments each topic slug has got)
-const createDocsRefObj = (docs) => {
+const createDocsRefObj = docs => {
   return docs.reduce((refObj, doc) => {
     if (refObj[doc.belongs_to] != undefined) {
       refObj[doc.belongs_to] += 1;
-    }
-    else {
+    } else {
       refObj[doc.belongs_to] = 1;
     }
-    return refObj
-  }, {})
-}
+    return refObj;
+  }, {});
+};
 
-const createAuthorRefObj = (docs) => {
-  console.log(docs[0].created_by)
+const createAuthorRefObj = docs => {
   return docs.reduce((refObj, doc) => {
     if (refObj[doc.created_by] != undefined) {
       refObj[doc.created_by] += 1;
-    }
-    else {
+    } else {
       refObj[doc.created_by] = 1;
     }
-    return refObj
-  }, {})
-}
+    return refObj;
+  }, {});
+};
 
-module.exports = { formatTopics, formatUsers, formatArticles, createRefObj, formatComments, commentCount, createDocsRefObj, createAuthorRefObj }
+module.exports = {
+  formatTopics,
+  formatUsers,
+  formatArticles,
+  createRefObj,
+  formatComments,
+  commentCount,
+  createDocsRefObj,
+  createAuthorRefObj
+};
